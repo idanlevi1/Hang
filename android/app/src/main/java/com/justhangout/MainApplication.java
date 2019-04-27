169f9252bd100b1ab77ff07c74d3e4f3023d1745
Idan Levi <idanlevi@moonsite.co.il>
2019-04-27 22:25:48 +0300

    react-native-share installaion ios&android

--- android/app/src/main/java/com/justhangout/MainApplication.java
+++ android/app/src/main/java/com/justhangout/MainApplication.java
@@ -3,6 +3,8 @@ package com.justhangout;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
+import cl.json.ShareApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
@@ -14,8 +16,13 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends Application implements ShareApplication, ReactApplication {
 
+  @Override
+  public String getFileProviderAuthority() {
+    return BuildConfig.APPLICATION_ID + ".provider";
+  }
+  
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -26,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSharePackage(),
             new LinearGradientPackage(),
             new RNDeviceInfo(),
             new RNI18nPackage()
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './Style';
 import { Button, HeaderParallax } from '../../components';
 import { flyColors } from '../../constants/Colors'
-// import Share from 'react-native-share'
+import Share from 'react-native-share'
 import Api from '../../server/Api'
 
 const apiUrlExample = 'https://1.base.maps.api.here.com/maptile/2.1/maptile/newest/normal.day/11/525/761/256/png8?output=base64&app_id=devportal-demo-20180625&app_code=9v2BkviRwi9Ot26kp2IysQ'
@@ -18,15 +18,16 @@ export default class HOME extends React.Component {
 
   }
 
-  onShare = async (file) => {
+  onShare = async () => {
     try {
       let response = await fetch(apiUrlExample, { timeout: 10000 })
       console.log('response', response)
-      const file = "data:image/jpeg;base64," + response._bodyText
-      this.onShare(file)
-      const shareOptions = { title: 'Share via', url: file };
+      const file = "data:image/jpeg;base64," + response._bodyText.substring(1,response._bodyText.length-2)
+      console.log(file)
+      const shareOptions = { title: 'Share via', urls: [file] };
       setTimeout(() => {
-        // Share.open(shareOptions)
+        Share.open(shareOptions)
+          .catch(err => console.log(err))
       }, 100);
     } catch (e) {
       console.log('on share error', e)
@@ -43,7 +44,7 @@ export default class HOME extends React.Component {
         title='Home'>
         <View style={{ backgroundColor: flyColors.background }}>
           <TouchableOpacity onPress={this.onShare}>
-            <Text>Share</Text>
+            <Text style={{fontSize:24, color: flyColors.header}}>Share</Text>
           </TouchableOpacity>
           {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(e => <Text key={e} style={{ color: flyColors.text, fontSize: 24, padding: 5, margin: 10, borderBottomWidth: 1 }}>{e}</Text>)}
         </View>
