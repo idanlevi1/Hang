687198eabaf7a68f60c8ee1f1491b7f032595f5f
idanlevi <idanlevi2@gmail.com>
2019-05-11 23:17:14 +0300

    installation

--- android/app/src/main/java/com/justhangout/MainApplication.java
+++ android/app/src/main/java/com/justhangout/MainApplication.java
@@ -3,6 +3,9 @@ package com.justhangout;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import cl.json.RNSharePackage;
 import cl.json.ShareApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -33,6 +36,9 @@ public class MainApplication extends Application implements ShareApplication, Re
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
             new RNSharePackage(),
             new LinearGradientPackage(),
             new RNDeviceInfo(),
